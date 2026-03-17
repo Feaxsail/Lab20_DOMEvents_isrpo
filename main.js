de48984aed8f16441ea993bb6ae03ac210cb78e8
whypikmi <ttelatnikova1@yandex.ru>
2026-03-17 14:11:12 +0300

    Step 4: modifying DOM content and styles

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-console.log(document);
-const header = document.getElementById('title');
-const paragraph = document.querySelector('.text');
-console.log(header);
-console.log(paragraph);
+console.log(document)
+const title = document.getElementById("title");
+const text = document.querySelector(".text");
+console.log(title);
+console.log(text);
+
+title.textContent = "DOM изменён через JS!";
+
+title.style.color = "blue";
+title.style.backgroundColor = "#f0f0f0";
+title.style.padding = "10px";
+
+text.style.fontSize = "18px";
+text.style.fontStyle = "italic";
+text.style.color = "darkgreen";
